6a51b62b63445c1972dbf94e9e1d8c1b6a705233
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-03-04 15:27:43 -0300

    Adicionando funções úteis em SQL

--- Comandos-uteis.sql
+++ Comandos-uteis.sql
@@ -1,6 +1,7 @@
-*****************************************Quantos registros existem na tabela do banco?
+**********************************Quantos registros existem na tabela do banco?********************************
 execute sp_spaceused NomeDaTabela
 
+***************************************************************************************************************
 
 **********************************DELETAR TODOS OS DADOS DO BANCO DE DADOS**************************************	
 1 - Desabilitar todas as constraints do banco de dados:
@@ -22,3 +23,42 @@ SELECT sobjects.name
 FROM sysobjects sobjects
 WHERE sobjects.xtype = 'U'
 ******************************************************************************************************************
+
+***********************************RETORNAR UM CAMPO QUE ACABOU DE SER INSERIDO***********************************
+INSERT INTO Pessoa (Id, Nome) OUTPUT INSERTED.Id VALUES (@Id, @Nome)
+
+******************************************************************************************************************
+
+***********************************TRUNCAR TABELA COM CHAVE ESTRANGEIRA*******************************************
+DELETE FROM Tabela DBCC CHECKIDENT ('Banco.dbo.Tabela',RESEED, 0)
+
+******************************************************************************************************************
+
+***********************************REMOVER LINHAS DUPLICADAS******************************************************
+DELETE FROM (SELECT *, row_number() OVER (partition by id order by id) as rn FROM mytable) WHERE rn > 1
+WITH CTE AS (SELECT *, row_number() OVER (partition by id order by id) as rn FROM mytable); //guarda os dados temporariamente
+DELETE FROM CTE WHERE rn > 1;
+
+*******************************************************************************************************************
+
+**********************************ATUALIZAR QUANTIDADE ESPECÍFICA DE REGISTRO**************************************
+WITH CTE AS 
+( 
+  SELECT TOP 100 * 
+  FROM TABELA T 
+  ORDER BY T.id 
+) 
+UPDATE CTE SET T.Name = 'foo'
+
+********************************************************************************************************************
+
+
+
+
+
+
+
+
+
+
+
